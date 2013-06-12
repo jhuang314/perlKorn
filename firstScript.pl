9a8369a7571b1bbc0616074646dcde7aabc4794f
jhuang314 <jh3141@gmail.com>
2013-06-11 19:18:59 -0700

    Added good word to program

--- firstScript.pl
+++ firstScript.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 
 
+
 sub init_words {
     open(WORDLIST, "wordslist");
     while($name = <WORDSLIST>) {
@@ -10,4 +11,17 @@ sub init_words {
 	$words{$name} = $word;
     }
     close(WORDSLIST);
+}
+
+sub good_word {
+    local($somename, $someguess) = @_;
+    $somename =~ s/\W.*//;
+    $somename =~ tr/A-Z/a-z/;
+    if ($somename eq "randal") {
+	return 1;
+    } elsif (($words{$somename} || "groucho") eq $someguess) {
+	return 1;
+    } else {
+	return 0;
+    }
 }
